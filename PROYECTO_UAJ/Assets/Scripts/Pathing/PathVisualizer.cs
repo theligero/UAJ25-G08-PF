5b7b6c61b52925aed7a3b18079353ec798c7aff1
Alberto Peñalba Martos <albpenal@ucm.es>
2025-05-17 11:44:25 +0200

    Pathfinder mejorado
    Se actualiza según te mueves recalculando la ruta y solo se activa al pulsar la letra L

--- PROYECTO_UAJ/Assets/Scripts/Pathing/PathVisualizer.cs
+++ PROYECTO_UAJ/Assets/Scripts/Pathing/PathVisualizer.cs
@@ -1,19 +1,34 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PathVisualizer : MonoBehaviour
 {
     [SerializeField]
-    private NavMeshAgent navMeshAgent;
-    [SerializeField]
     private LineRenderer lineRenderer;
+    [SerializeField]
+    private Transform objetivo;
+
+
+    private NavMeshPath path;
+    void Start()
+    {
+        Debug.Log("Para activar la línea de pathing pulsa L");
+        path = new NavMeshPath();
+    }
 
     void Update()
     {
-        if (navMeshAgent.hasPath)
+        if (objetivo != null)
         {
-            lineRenderer.positionCount = navMeshAgent.path.corners.Length;
-            lineRenderer.SetPositions(navMeshAgent.path.corners);
+            // Calcula una ruta desde tu posición actual hasta el objetivo
+            if (Input.GetKey(KeyCode.L) && NavMesh.CalculatePath(transform.position, objetivo.position, NavMesh.AllAreas, path))
+            {
+                lineRenderer.positionCount = path.corners.Length;
+                lineRenderer.SetPositions(path.corners);
+            }
+            else lineRenderer.positionCount = 0;
         }
+        else objetivo = GameObject.FindWithTag("Objetivo").transform;
     }
 }
