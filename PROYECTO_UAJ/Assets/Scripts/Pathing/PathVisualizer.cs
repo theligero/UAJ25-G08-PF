c0cab0ae689817af095d553c3cbfe85255959b00
Alberto Peñalba Martos <albpenal@ucm.es>
2025-05-19 11:22:43 +0200

    mapeo a mando

--- PROYECTO_UAJ/Assets/Scripts/Pathing/PathVisualizer.cs
+++ PROYECTO_UAJ/Assets/Scripts/Pathing/PathVisualizer.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.LowLevel;
 
 public class PathVisualizer : MonoBehaviour
 {
@@ -9,20 +10,22 @@ public class PathVisualizer : MonoBehaviour
     [SerializeField]
     private Transform objetivo;
 
+    private bool active = false;
 
     private NavMeshPath path;
     void Start()
     {
-        Debug.Log("Para activar la línea de pathing pulsa L");
+        Debug.Log("Para activar/desactivar la línea de pathing pulsa L o el botón oeste del mando");
         path = new NavMeshPath();
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.L) || Input.GetButtonDown("Fire3")) active = !active;
         if (objetivo != null)
         {
             // Calcula una ruta desde tu posición actual hasta el objetivo
-            if (Input.GetKey(KeyCode.L) && NavMesh.CalculatePath(transform.position, objetivo.position, NavMesh.AllAreas, path))
+            if (active && NavMesh.CalculatePath(transform.position, objetivo.position, NavMesh.AllAreas, path))
             {
                 lineRenderer.positionCount = path.corners.Length;
                 lineRenderer.SetPositions(path.corners);
